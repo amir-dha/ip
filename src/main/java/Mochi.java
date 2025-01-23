d26a1fdbb0baba469695752c56325daf514c530f
amir-dha <ravichandranamirdha25@gmail.com>
2025-01-23 21:42:13 +0800

    This is Level 2.

--- src/main/java/Mochi.java
+++ src/main/java/Mochi.java
@@ -5,6 +5,10 @@ public class Mochi {
         Scanner sc = new Scanner(System.in);
         String line = "------------------------------------------------";
         String botName = "Mochi";
+        String[] tasks = new String[100];
+        int taskCounter = 0;
+
+
         System.out.println(" " + line);
         System.out.println(" Hello! I'm " + botName);
         System.out.println(" What can I do for you?");
@@ -18,10 +22,19 @@ public class Mochi {
                 System.out.println(" " + "Bye. Hope to see you again soon!");
                 System.out.println(" " + line);
                 break;
+            } else if (input.equals("list")) {
+                System.out.println(line);
+                for (int i = 0; i < taskCounter; i++) {
+                    System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                }
+                System.out.println(line);
+            } else {
+                tasks[taskCounter] = input;
+                taskCounter++;
+                System.out.println(" " + line);
+                System.out.println(" added: " + input);
+                System.out.println(" " + line);
             }
-            System.out.println(" " + line);
-            System.out.println(" " + input);
-            System.out.println(" " + line);
         }
         sc.close();
     }
