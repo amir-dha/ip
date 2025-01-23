efe14d1f62d4ffdf932fc5450d373b91692c966d
amir-dha <ravichandranamirdha25@gmail.com>
2025-01-23 17:22:30 +0800

    This is Level 0.

---
+++ src/main/java/Mochi.java
@@ -0,0 +1,10 @@
+public class Mochi {
+    public static void main(String[] args) {
+        String botName = "Mochi";
+        System.out.println("------------------------------------------------");
+        System.out.println("Hello! I'm " + botName);
+        System.out.println("What can I do for you?");
+        System.out.println("------------------------------------------------");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
