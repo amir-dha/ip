b0f1d7bf4d0a1a04ea0153979859475d4700488d
amir-dha <ravichandranamirdha25@gmail.com>
2025-01-24 13:29:24 +0800

    This is Level 6.

--- src/main/java/Mochi.java
+++ src/main/java/Mochi.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Mochi {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String line = " ------------------------------------------------";
+        String line = " ----------------------------------------------------------------------------";
         String botName = "Mochi";
         ArrayList<Task> tasks = new ArrayList<>();
 
@@ -32,8 +32,14 @@ public class Mochi {
                     }
                     System.out.println(line);
 
-                } else if (input.startsWith("mark ")) {
+                } else if (input.startsWith("mark")) {
+                    if (input.length() <= 4 || input.substring(5).trim().isEmpty()) {
+                        throw new MochiException(" Oi, which task to mark? Can give me number or not?");
+                    }
                     int taskInd = Integer.parseInt(input.substring(5)) - 1;
+                    if (taskInd < 0 || taskInd >= tasks.size()) {
+                        throw new MochiException(" Babes, this task don't even exist?? How to mark?");
+                    }
                     tasks.get(taskInd).markAsDone();
                     System.out.println(line);
                     System.out.println(" Wow. You actually did something, that's one down I guess. " +
@@ -41,8 +47,14 @@ public class Mochi {
                     System.out.println(" " + tasks.get(taskInd));
                     System.out.println(line);
 
-                } else if (input.startsWith("unmark ")) {
+                } else if (input.startsWith("unmark")) {
+                    if (input.length() <= 6 || input.substring(7).trim().isEmpty()) {
+                        throw new MochiException(" Great, now you want me to read your mind to unmark a task.");
+                    }
                     int taskInd = Integer.parseInt(input.substring(7)) - 1;
+                    if (taskInd < 0 || taskInd >= tasks.size()) {
+                        throw new MochiException(" Babes, this task don't even exist?? How to unmark?");
+                    }
                     tasks.get(taskInd).markAsNotDone();
                     System.out.println(line);
                     System.out.println(" Sigh, it was my fault to think you actually finished a task..");
@@ -83,11 +95,11 @@ public class Mochi {
 
                 } else if (input.startsWith("event")) {
                     if (input.length() <= 5) {
-                        throw new MochiException("Babes, events need a description, a '/from' time and a '/to' time");
+                        throw new MochiException(" Babes, events need a description, a '/from' time and a '/to' time");
                     }
                     String[] p = input.substring(6).split("/from | /to");
                     if (p.length < 3 || p[0].isEmpty() || p[1].isEmpty() || p[2].isEmpty()) {
-                        throw new MochiException("Babes, events need a description, a '/from' time and a '/to' time");
+                        throw new MochiException(" Babes, events need a description, a '/from' time and a '/to' time");
                     }
                     Task task = new Event(p[0], p[1], p[2]);
                     tasks.add(task);
@@ -96,6 +108,26 @@ public class Mochi {
                     System.out.println("  " + task);
                     System.out.println(" Now you got " + tasks.size() + " tasks in the list.");
                     System.out.println(line);
+
+                } else if (input.startsWith("delete")) {
+                    if (input.length() <= 6 || input.substring(7).trim().isEmpty()) {
+                        throw new MochiException(" Great, now you cant't even delete tasks.. " +
+                                "Bro, tell me which task to delete.");
+                    }
+                    try {
+                        int taskInd = Integer.parseInt(input.substring(7).trim()) - 1;
+                        if (taskInd < 0 || taskInd >= tasks.size()) {
+                            throw new MochiException(" Babes this task doesn't exist. Give me a task that exists.");
+                        }
+                        Task task = tasks.remove(taskInd);
+                        System.out.println(line);
+                        System.out.println(" Can. Take out task already.");
+                        System.out.println(" " + task);
+                        System.out.println(" Now you got " + tasks.size() + " tasks in the list.");
+                        System.out.println(line);
+                    } catch (NumberFormatException e) {
+                        throw new MochiException(" Eh can give me valid number to delete or not?!");
+                    }
 
                 } else {
                     throw new MochiException(" Bro nani are you saying? Can you please make sense? Jebal!!");
