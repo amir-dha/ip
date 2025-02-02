cf7206cc84040c7664a22b39839968206c536b88
amir-dha <ravichandranamirdha25@gmail.com>
2025-02-03 01:47:09 +0800

    Added JUnit tests for Mochi and TaskList

---
+++ src/test/java/mochi/MochiTest.java
@@ -0,0 +1,57 @@
+package mochi;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MochiTest {
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    private void simulateInput(String data) {
+        System.setIn(new ByteArrayInputStream(data.getBytes()));
+    }
+
+    @BeforeEach
+    void setUpStream() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @Test
+    void testAddTodo() {
+        simulateInput("todo read book\nlist\nbye\n");
+        Mochi.main(new String[]{});
+        assertTrue(outContent.toString().contains(" [T][ ] read book"));
+    }
+
+    @Test
+    void testAddDeadline() {
+        simulateInput("deadline finish assignment /by 2024-07-02 1800\nlist\nbye\n");
+        Mochi.main(new String[]{});
+        assertTrue(outContent.toString().contains(" [D][ ] finish assignment (by: 2 Jul 2024, 6:00 PM)"));
+    }
+
+    @Test
+    void testAddEvent() {
+        simulateInput("event party /from 2024-07-02 1800 /to 2024-07-02 2300\nlist\nbye\n");
+        Mochi.main(new String[]{});
+        assertTrue(outContent.toString().contains(" [E][ ] party (from: 2 Jul 2024, 6:00 PM to: 2 Jul 2024, 11:00 PM)"));
+    }
+
+    @Test
+    void testMarkAsDone() {
+        simulateInput("mark 1\nlist\nbye\n");
+        Mochi.main(new String[]{});
+        assertTrue(outContent.toString().contains(" [T][X] read book"));
+    }
+
+    @Test
+    void testUnmarkAsDone() {
+        simulateInput("unmark 1\nlist\nbye\n");
+        Mochi.main(new String[]{});
+        assertTrue(outContent.toString().contains(" [T][ ] read book"));
+    }
+}
